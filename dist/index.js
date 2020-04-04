e410c13d7a5e7cb16bc36cd12e5a1dbd9890fcfd
Emiliano Errecalde <eerrecalde@gmail.com>
2020-04-05 00:14:08 +0100

    Fixed issue with imported unexisting components

--- dist/index.js
+++ dist/index.js
@@ -21,27 +21,11 @@ Object.defineProperty(exports, "GridItem", {
     return _gridItem.default;
   }
 });
-Object.defineProperty(exports, "ItemDetails", {
-  enumerable: true,
-  get: function get() {
-    return _itemDetails.default;
-  }
-});
-Object.defineProperty(exports, "ItemForm", {
-  enumerable: true,
-  get: function get() {
-    return _itemForm.default;
-  }
-});
 
 var _favorite = _interopRequireDefault(require("./favorite/favorite"));
 
 var _grid = _interopRequireDefault(require("./grid/grid.view"));
 
 var _gridItem = _interopRequireDefault(require("./grid-item/grid-item"));
-
-var _itemDetails = _interopRequireDefault(require("./item-details/item-details"));
-
-var _itemForm = _interopRequireDefault(require("./item-form/item-form"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
 import Favorite from './favorite/favorite';
 import GridView from './grid/grid.view';
 import GridItem from './grid-item/grid-item';
-import ItemDetails from './item-details/item-details';
-import ItemForm from './item-form/item-form';
 
-export { Favorite, GridItem, GridView, ItemDetails, ItemForm };
+export { Favorite, GridItem, GridView };
